04ab537ecf9ee6a7d6bbdf6e900d6bc88cc979c7
TuanVT <vutrongtuan@gmail.com>
2024-11-28 15:57:27 +0700

    ff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,8 +29,8 @@ export default function App({Component, pageProps}: AppPropsWithLayout) {
 	return (
 		<Fragment>
 			<Head>
-				<title>Hệ thống quản trị</title>
-				<meta name='description' content='Hệ thống quản trị' />
+				<title>Hệ thống PM</title>
+				<meta name='description' content='Hệ thống PM' />
 				<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale = 1.0' />
 			</Head>
 			<AppProvider pageProps={pageProps}>{getLayout(<Component {...pageProps} />)}</AppProvider>
