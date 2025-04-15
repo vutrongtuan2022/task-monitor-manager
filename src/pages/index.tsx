7db7e201ad8eda50e1f041d00314ddaf18f0bbe9
dang truong <dangtruong1609.work@gmail.com>
2025-04-15 14:47:10 +0700

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Page() {
 		<Fragment>
 			<Head>
 				<title>Quản lý dự án</title>
-				<meta name='description' content='Quản lý dự án vietinbank' />
+				<meta name='description' content='Quản lý dự án' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
